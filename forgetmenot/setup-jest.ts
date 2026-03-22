3b7340538743896b14efe30af6cbb73eb4f6782a
jansuria <jansuria1@gmail.com>
2026-03-21 22:42:51 -0400

    Initial creation of app, ngrx and jest dependencies added, title created, input added

---
+++ forgetmenot/setup-jest.ts
@@ -0,0 +1,8 @@
+import 'jest-preset-angular/setup-env/zone';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
