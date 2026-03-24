e5eb58765a77879ded4d95a9480dbae41ad145f1
jansuria <jansuria1@gmail.com>
2026-03-23 20:06:36 -0400

    Implemented delete and get notes feature

--- forgetmenot/src/app/components/state/note.actions.ts
+++ forgetmenot/src/app/components/state/note.actions.ts
@@ -3,8 +3,18 @@ import { Note } from '../models/note.model';
 
 export const getNotesRequest = createAction('[Note] Get user notes', props<{ userId: string }>());
 
+export const getNotesSuccess = createAction(
+  '[Note] Retrieved Successully',
+  props<{ notes: Note[] }>(),
+);
+
 export const createNoteRequest = createAction(
   '[Note] Create Request',
+  props<{ userId: string; note: string }>(),
+);
+
+export const createNoteSuccess = createAction(
+  '[Note] Created Succesfully',
   props<{ userId: string; note: string }>(),
 );
 
@@ -13,7 +23,7 @@ export const deleteNoteReqeust = createAction(
   props<{ userId: string; note: string }>(),
 );
 
-export const createNoteSuccess = createAction(
-  '[Note] Created Succesfully',
+export const deleteNoteSuccess = createAction(
+  '[Note] Delete Request Success',
   props<{ userId: string; note: string }>(),
 );
